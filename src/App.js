c511dd93401e6f271ff7cf78bd1023601c3d4463
Sergey Zykov <zykau_s@mail.ru>
2019-07-01 18:39:06 +0300

    feat: add a basic folder structure

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 
 import React, { Component, } from 'react'
 import { StyleSheet, Text, View, } from 'react-native'
+import Comp from 'src/components'
 
 type Props = {}
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to my app</Text>
+        <Comp />
         <Text style={styles.instructions}>To get started, edit App.js</Text>
       </View>
     )
---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import React, { Component, } from 'react'
+import {  Text, View, } from 'react-native'
+
+export default class Comp extends Component<Props> {
+  render() {
+    return (
+      <View >
+        <Text s>Hello module!!!!!!!!!!!!!</Text>
+      </View>
+    )
+  }
+}
